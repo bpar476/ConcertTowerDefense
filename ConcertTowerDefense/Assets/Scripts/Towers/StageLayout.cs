9bcdecb275559ddd724a67aa045b225cabe8d5ab
Ben Partridge <ben.partridge249@gmail.com>
2020-12-07 09:09:36 +1100

    Implement grabbing tower from selection screen and it changing colour based on whether it is hovering over a valid placement position

---
+++ ConcertTowerDefense/Assets/Scripts/Towers/StageLayout.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StageLayout : MonoBehaviour
+{
+    /// <summary>
+    /// Game objects representing the legal positions for a tower
+    /// to be placed
+    /// </summary>
+    [SerializeField]
+    private GameObject[] towerLocations;
+
+    /// <summary>
+    /// Half the width of the cell. Cells are square so this is
+    /// also half the height;
+    /// </summary>
+    [SerializeField]
+    private float cellRadius;
+
+    private Cell[] cachedLayout;
+
+    public Cell[] Layout
+    {
+        get
+        {
+            if (cachedLayout == null)
+            {
+                cachedLayout = towerLocations.Select(obj =>
+             {
+                 var x = obj.transform.position.x;
+                 var y = obj.transform.position.y;
+                 var cell = new Cell();
+                 cell.bottomLeft = new Vector2(x - cellRadius, y - cellRadius);
+                 cell.topRight = new Vector2(x + cellRadius, y + cellRadius);
+                 return cell;
+             }).ToArray();
+            }
+
+            return cachedLayout;
+        }
+    }
+
+    public struct Cell
+    {
+        public Vector2 bottomLeft;
+        public Vector3 topRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (var cell in Layout)
+        {
+            var center = new Vector3(cell.bottomLeft.x + cellRadius, cell.bottomLeft.y + cellRadius);
+            Gizmos.DrawWireCube(center, new Vector3(cellRadius * 2, cellRadius * 2, 0));
+        }
+    }
+}
---
+++ ConcertTowerDefense/Assets/Scripts/Towers/TowerPlacer.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+public class TowerPlacer : MonoBehaviour
+{
+    /// <summary>
+    /// Color to tint the tower's sprite when it can be placed in its current position
+    /// </summary>
+    [SerializeField]
+    private Color legalPlacementTint;
+    /// <summary>
+    /// Color to tint the tower's sprite when it cannot be placed in its current position
+    /// </summary>
+    [SerializeField]
+    private Color illegalPlacementTint;
+
+    /// <summary>
+    /// The stage that the towers can be placed on
+    /// </summary>
+    [SerializeField]
+    private StageLayout stage;
+
+    private Camera mainCam;
+    private GameObject currentTower;
+    private Color originalColor;
+
+    private Mode state;
+    private bool hoveringValidCell = false;
+    private StageLayout.Cell currentCell;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        state = Mode.EMPTY;
+    }
+
+    private void Update()
+    {
+        if (state == Mode.HOLDING)
+        {
+            var currentPos = MousePositionToGameWorldPosition();
+            currentTower.transform.position = currentPos;
+
+            foreach (var cell in stage.Layout)
+            {
+                if (currentPos.x >= cell.bottomLeft.x && currentPos.x <= cell.topRight.x && currentPos.y >= cell.bottomLeft.y && currentPos.y <= cell.topRight.y)
+                {
+                    currentTower.GetComponent<SpriteRenderer>().color = originalColor * legalPlacementTint;
+                    currentCell = cell;
+                    hoveringValidCell = true;
+                    break;
+                }
+
+                hoveringValidCell = false;
+            }
+
+            if (hoveringValidCell)
+            {
+                // TODO cache renderer
+                currentTower.GetComponent<SpriteRenderer>().color = originalColor * legalPlacementTint;
+            }
+            else
+            {
+                currentTower.GetComponent<SpriteRenderer>().color = originalColor * illegalPlacementTint;
+
+            }
+        }
+    }
+
+    public void GrabTower(GameObject tower)
+    {
+        if (state == Mode.EMPTY)
+        {
+            currentTower = Instantiate(tower, MousePositionToGameWorldPosition(), Quaternion.identity);
+            var renderer = currentTower.GetComponent<SpriteRenderer>();
+            originalColor = renderer.color;
+            renderer.color = originalColor * illegalPlacementTint;
+            state = Mode.HOLDING;
+        }
+    }
+
+    public void DropTower()
+    {
+        if (state == Mode.HOLDING)
+        {
+
+        }
+    }
+
+    private Vector3 MousePositionToGameWorldPosition()
+    {
+        var point = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        return new Vector3(point.x, point.y, 0);
+    }
+
+    private enum Mode
+    {
+        EMPTY, HOLDING
+    }
+}

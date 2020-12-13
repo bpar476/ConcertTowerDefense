0f2bcc2bc8071cd1d65a77be744c2d6543fae208
Ben Partridge <ben.partridge249@gmail.com>
2020-12-14 08:33:47 +1100

    Implement notification system

---
+++ ConcertTowerDefense/Assets/Scripts/UI/NotificationSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationSystem : MonoBehaviour
+{
+    private static readonly string ANIM_TRIGGER_NOTIFY = "Notify";
+    private static readonly string ANIM_STATE_IDLE = "Idle";
+
+    private Animator animator;
+    private TMPro.TMP_Text textUI;
+
+    private Queue<string> notificationQueue;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        textUI = GetComponent<TMPro.TMP_Text>();
+        notificationQueue = new Queue<string>();
+    }
+
+    public void PublishNotification(string notificationText)
+    {
+        notificationQueue.Enqueue(notificationText);
+    }
+
+    private void Update()
+    {
+        if (notificationQueue.Count > 0 && animator.GetCurrentAnimatorStateInfo(0).IsName(ANIM_STATE_IDLE))
+        {
+            var text = notificationQueue.Dequeue();
+            textUI.text = text;
+
+            animator.SetTrigger(ANIM_TRIGGER_NOTIFY);
+        }
+    }
+}
---
+++ ConcertTowerDefense/Assets/Scripts/UI/TestNotifications.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Test class which queues three notifications on start. If the notification system is working properly, it 
+/// should play all three notifications, one after the other.
+/// </summary>
+public class TestNotifications : MonoBehaviour
+{
+    public NotificationSystem notification;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            notification.PublishNotification(string.Format("Notification {0}", i));
+        }
+    }
+}

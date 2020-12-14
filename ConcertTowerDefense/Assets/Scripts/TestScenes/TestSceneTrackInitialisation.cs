18dada02708ec563dfc164ed0677822ccb8b01cd
Ben Partridge <ben.partridge249@gmail.com>
2020-12-15 08:01:17 +1100

    Fix up tests scenes for playing with different instrument tracks

---
+++ ConcertTowerDefense/Assets/Scripts/TestScenes/TestSceneTrackInitialisation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSceneTrackInitialisation : MonoBehaviour
+{
+    [SerializeField]
+    private TrackPlayer player;
+
+    [SerializeField]
+    private MusicTrack[] tracks;
+
+    [SerializeField]
+    private BeatMappedShooter tower;
+
+    private void Start()
+    {
+        player.LoadTracks(tracks, 1);
+        tower.RegisterOnBeatCallback(player.Mapper);
+    }
+}

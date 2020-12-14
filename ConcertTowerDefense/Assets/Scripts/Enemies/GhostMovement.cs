d92535c67bdd28434ff1569ca42b0eb46fc942cc
Ben Partridge <ben.partridge249@gmail.com>
2020-12-15 08:52:01 +1100

    Implement basic ghost movement and scheduled spawning of ghosts per lane

---
+++ ConcertTowerDefense/Assets/Scripts/Enemies/GhostMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    /// <summary>
+    /// Horizontal movement speed of the ghost in units per second
+    /// </summary>
+    [SerializeField]
+    private float moveSpeed = 1;
+
+    private void Update()
+    {
+        transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ ConcertTowerDefense/Assets/Scripts/Enemies/GhostSpawner.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class GhostSpawner : MonoBehaviour
+{
+
+    /// <summary>
+    /// Whether the spawner has spawned all of the ghosts it plans to spawn
+    /// </summary>
+    /// <value>True if there are no more ghosts to spawn. False otherwise</value>
+    public bool Finished { get { return nextSpawnIndex >= spawnTimings.Length; } }
+
+    [SerializeField]
+    private GameObject ghostPrefab;
+
+    [SerializeField]
+    private float[] spawnTimings;
+
+    /// <summary>
+    /// If true, the spawner will start spawning ghosts on scene load. Otherwise it will wait until
+    /// another object calls StartSpawner
+    /// </summary>
+    [SerializeField]
+    private bool startImmediately;
+
+    private int nextSpawnIndex = 0;
+
+    private bool started = false;
+
+    private float startTime;
+
+    private void Start()
+    {
+        if (startImmediately)
+        {
+            StartSpawner();
+        }
+    }
+
+    public void StartSpawner()
+    {
+        started = true;
+        startTime = Time.timeSinceLevelLoad;
+    }
+
+    void Update()
+    {
+        if (nextSpawnIndex < spawnTimings.Length && started)
+        {
+            if (Time.timeSinceLevelLoad - startTime > spawnTimings[nextSpawnIndex])
+            {
+                SpawnGhost();
+                nextSpawnIndex++;
+            }
+        }
+    }
+
+    private void SpawnGhost()
+    {
+        Instantiate(ghostPrefab, transform.position, Quaternion.identity);
+    }
+}

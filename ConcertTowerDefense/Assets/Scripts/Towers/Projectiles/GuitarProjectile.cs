6cc97761e6ab9893780deb57e07c393c1f3dedff
Ben Partridge <ben.partridge249@gmail.com>
2020-12-22 08:47:25 +1100

    Implement simple guitar projectile

---
+++ ConcertTowerDefense/Assets/Scripts/Towers/Projectiles/GuitarProjectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GuitarProjectile : ProjectileParent
+{
+
+    [SerializeField]
+    private float damage;
+
+    public override void Hit(GameObject ghost)
+    {
+        ghost.GetComponent<GhostHealth>().Damage(damage);
+    }
+}

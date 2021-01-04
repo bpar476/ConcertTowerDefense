abee896fbd9fff19ba279d0d4b5839cd3054ad4b
Ben Partridge <ben.partridge249@gmail.com>
2021-01-04 17:14:13 +1100

    Make music note projectiles flash between two colours while moving

---
+++ ConcertTowerDefense/Assets/Scripts/Util/RandomBetweenTwoColours.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RandomBetweenTwoColours : MonoBehaviour
+{
+    [SerializeField]
+    private Color from;
+
+    [SerializeField]
+    private Color to;
+
+    private new SpriteRenderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        renderer.color = Color.Lerp(from, to, Random.Range(0f, 1.0f));
+    }
+}
